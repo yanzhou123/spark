c283530180f3f1a1409673f15460b58fc62490d4
yanzhou123 <yanzhou0362@gmail.com>
2016-09-26 20:39:11 -0700

    cleanup of SharedState

--- sql/core/src/main/scala/org/apache/spark/sql/internal/SharedState.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/internal/SharedState.scala
@@ -118,35 +118,6 @@ private[sql] class SharedState(val sparkContext: SparkContext) extends Logging {
 
 object SharedState {
 
-  private val HIVE_EXTERNAL_CATALOG_CLASS_NAME = "org.apache.spark.sql.hive.HiveExternalCatalog"
-
-  private def externalCatalogClassName(conf: SparkConf): String = {
-    conf.get(CATALOG_IMPLEMENTATION) match {
-      case "hive" => HIVE_EXTERNAL_CATALOG_CLASS_NAME
-      case "in-memory" => classOf[InMemoryCatalog].getCanonicalName
-    }
-  }
-
-  /**
-   * Helper method to create an instance of [[T]] using a single-arg constructor that
-   * accepts an [[Arg1]] and an [[Arg2]].
-   */
-  private def reflect[T, Arg1 <: AnyRef, Arg2 <: AnyRef](
-      className: String,
-      ctorArg1: Arg1,
-      ctorArg2: Arg2)(
-      implicit ctorArgTag1: ClassTag[Arg1],
-      ctorArgTag2: ClassTag[Arg2]): T = {
-    try {
-      val clazz = Utils.classForName(className)
-      val ctor = clazz.getDeclaredConstructor(ctorArgTag1.runtimeClass, ctorArgTag2.runtimeClass)
-      val args = Array[AnyRef](ctorArg1, ctorArg2)
-      ctor.newInstance(args: _*).asInstanceOf[T]
-    } catch {
-      case NonFatal(e) =>
-        throw new IllegalArgumentException(s"Error while instantiating '$className':", e)
-    }
-  }
 }
 
 
